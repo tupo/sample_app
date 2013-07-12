119930a4247ffce290178dc753f24258579f6693
Tupo <tupo@wanderstheworld.com>
2013-07-12 00:26:01 -0400

    Finish layout and routes

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130710231040) do
+ActiveRecord::Schema.define(version: 20130711013459) do
 
   create_table "users", force: true do |t|
     t.string   "name"
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
+    t.string   "string"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
